356236d68aed5ef1fd781156dc3b457fc8c3940f
gia <gianebere@gmail.com>
2026-09-23 21:13:56 -0500

    regularizacion de pipelines

--- electrocasa/resources/pipelines/gold/dotacion_sucursal.py
+++
@@ -1,7 +0,0 @@
-from pyspark import pipelines as dp
-from src.electrocasa.transforms.gold.dotacion_sucursal import transform_dotacion_sucursal
-
-
-@dp.materialized_view(name="dotacion_sucursal", comment="Dotacion activa de empleados por sucursal", table_properties={"quality": "gold"})
-def dotacion_sucursal():
-    return spark.read.table("silver.empleados_historial").transform(transform_dotacion_sucursal)
